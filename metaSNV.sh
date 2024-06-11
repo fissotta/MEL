104223501fba4258a16a15edb31be5ac7559b31c
Francisco Issotta <53593248+fissotta@users.noreply.github.com>
2024-06-11 16:09:47 -0400

    Update metaSNV.sh

--- metaSNV.sh
+++ metaSNV.sh
@@ -1,3 +1,78 @@
+####################
+### Installation ###
+#####################
+
+mamba create --name metaSNV -c bioconda -c conda-forge 'metasnv>=2.0.1'
+
+https://github.com/metasnv-tool/metaSNV/tree/master
+
+mamba activate metaSNV
+
+#############
+### FIX 1 ###
+#############
+
+download htslib-1.11 @ https://sourceforge.net/projects/samtools/files/
+
+cd htslib-1.11 & make
+
+#download samtools-1.11 @ https://sourceforge.net/projects/samtools/files/ (if you download or clone the last version, it won't work because qaTools requires a library called sam.h that's renamed in later versions)
+
+cd samtools-1.11 & make
+
+git clone https://github.com/CosteaPaul/qaTools.git
+
+#edit qaTools' Makefile as described in https://github.com/CosteaPaul/qaTools so that HTSLIB and SAMTOOLS point at the htslib-1.11 and samtools-1.11 paths
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/path/to/htslib-1.11"
+
+cd qaTools & make
+
+#change
+
+ cmd = ['{}/src/qaTools/qaCompute'.format(basedir),
+#to
+
+    cmd = ['/path/to/qaTools/qaCompute',
+in /path/to/anaconda3/share/metasnv-2.0.4/metaSNV.py
+
+
+
+#############
+### FIX 2 ###
+#############
+
+# have solved this problem.
+# Find the location of the script "metaSNV_subpopr.R" and move the line 365 above the line 358:
+# Before:
+
+ ~/mambaforge/envs/metaSNV/share/metasnv-2.0.4/metaSNV_subpopr.R
+ 
+bpParam <- MulticoreParam(workers = min(N.CORES,length(species)),
+                          jobname = "subpopr",
+                          stop.on.error = FALSE,
+                          threshold = "DEBUG",
+                          log = TRUE,
+                          progressbar = printProgressBar,
+                          logdir = paste0(OUT.DIR,"/threadLogs"))
+dir.create(paste0(OUT.DIR,"/threadLogs"), recursive = T, showWarnings = FALSE)
+
+# After:
+
+dir.create(paste0(OUT.DIR,"/threadLogs"), recursive = T, showWarnings = FALSE)
+bpParam <- MulticoreParam(workers = min(N.CORES,length(species)),
+                          jobname = "subpopr",
+                          stop.on.error = FALSE,
+                          threshold = "DEBUG",
+                          log = TRUE,
+                          progressbar = printProgressBar,
+                          logdir = paste0(OUT.DIR,"/threadLogs"))
+
+
+######################### 
+### Input preparation ### 
+######################### 
+
 #FR with coverm
 coverm genome --methods length covered_bases count mean relative_abundance rpkm --min-read-aligned-length 45 --min-read-percent-identity 97 --min-covered-fraction 0 --discard-unmapped --output-format sparse --genome-fasta-files references/NC_009767.fna --genome-fasta-extension fna --bam-file-cache-directory NC_009767_MGTA --threads 90 --output-file NC_009767_MGTA.txt -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto10_GGTCACGA-GTATTATG_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto10_GGTCACGA-GTATTATG_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto11_48_dna_GAATCCGTGG-TCACATGAGA_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto11_48_dna_GAATCCGTGG-TCACATGAGA_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto11_62_dna_ACGTGAACAT-ACTAGCCGTG_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto11_62_dna_ACGTGAACAT-ACTAGCCGTG_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto11_CTGCTTCC-GATAGATC_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto11_CTGCTTCC-GATAGATC_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto12_TCATCCTT-AGCGAGCT_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto12_TCATCCTT-AGCGAGCT_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto13_AGGTTATA-CAGTTCCG_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto13_AGGTTATA-CAGTTCCG_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto1_CAATTAAC-CGAGATAT_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto1_CAATTAAC-CGAGATAT_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto2_TGGCCGGT-TAGAGCGC_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto2_TGGCCGGT-TAGAGCGC_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto3_AGTACTCC-AACCTGTT_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto3_AGTACTCC-AACCTGTT_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto4_GACGTCTT-GGTTCACC_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto4_GACGTCTT-GGTTCACC_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto5_TGCGAGAC-CATTGTTG_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto5_TGCGAGAC-CATTGTTG_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto6_CATAGAGT-TGCCACCA_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto6_CATAGAGT-TGCCACCA_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto7_ACAGGCGC-CTCTGCCT_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto7_ACAGGCGC-CTCTGCCT_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto8_GTGAATAT-TCTCATTC_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto8_GTGAATAT-TCTCATTC_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto9_AACTGTAG-ACGCCGCA_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto9_AACTGTAG-ACGCCGCA_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto_3_45_dna_CTCGAAGGAA-CGAGGCCTAT_L001_R1_001_TRIM.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/Pto_3_45_dna_CTCGAAGGAA-CGAGGCCTAT_L001_R2_001_TRIM.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/T60_TAT2020_1.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/T60_TAT2020_2.fq -1 ../DATA_PD/MGTA/TRIMMED/PAIRED/T82_TAT2020_1.fq -2 ../DATA_PD/MGTA/TRIMMED/PAIRED/T82_TAT2020_2.fq
 
@@ -27,7 +102,7 @@ for f in *sorted.bam; do samtools index -@ 30 $f; done
 #annotations gtf to gff3
 parallel --jobs 4 ' agat_convert_sp_gxf2gxf.pl -g {} -o {}.gff3' ::: *gtf
 
-#edit gff3 solo dejar 1 commen tline
+#edit gff3 solo dejar 1 comment tline
 nano gff2metaSNV_annotation.py
 
 ##gff-version 3
@@ -43,38 +118,13 @@ input_gff            = r"/media/issotta/SSDWORK/loutraki/reference_genomes/ncbi_
 output_folder        = r"/media/issotta/SSDWORK/loutraki/reference_genomes/ncbi_dataset/data/dbann/NC_010175"
 
 
+#########################
+### Execution metaSNV ###
+#########################
 
+#activcate env
+mamba activate metaSNV
 
-#### FIX ###
-#### FIX ###
-
-#download htslib-1.11 @ https://sourceforge.net/projects/samtools/files/
-
-cd htslib-1.11 & make
-
-#download samtools-1.11 @ https://sourceforge.net/projects/samtools/files/ (if you download or clone the last version, it won't work because qaTools requires a library called sam.h that's renamed in later versions)
-
-cd samtools-1.11 & make
-
-git clone https://github.com/CosteaPaul/qaTools.git
-
-#edit qaTools' Makefile as described in https://github.com/CosteaPaul/qaTools so that HTSLIB and SAMTOOLS point at the htslib-1.11 and samtools-1.11 paths
-
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/path/to/htslib-1.11"
-
-cd qaTools & make
-
-#change
-
- cmd = ['{}/src/qaTools/qaCompute'.format(basedir),
-#to
-
-    cmd = ['/path/to/qaTools/qaCompute',
-in /path/to/anaconda3/share/metasnv-2.0.4/metaSNV.py
-
-
-#### FIX ###
-#### FIX ###
 
 #faidx references
 samtools faidx NC_010175.fna
@@ -85,8 +135,6 @@ samtools faidx NC_010175.fna
 /media/antiguo_stark/Dilanaz/TEST_metaSNV/VAN18202NCBI_S82020S_sorted.bam
 /media/antiguo_stark/Dilanaz/TEST_metaSNV/VAN18202NCBI_S82020_sorted.bam
 
-#activcate env
-conda activate metaSNV
 
 #run metaSNV
 metaSNV.py --threads 30  NC_010175_metaSNV NC_010175_bam.list references/NC_010175.fna --db_ann metaSNV_anntotations/NC_010175_metaSNV_anntotations.txt
@@ -94,38 +142,5 @@ metaSNV.py --threads 30  NC_010175_metaSNV NC_010175_bam.list references/NC_0101
 metaSNV_Filtering.py --n_threads 30 NC_010175_metaSNV
 
 metaSNV_DistDiv.py --n_threads 30 --dist --div --divNS --matched --filt NC_010175_metaSNV/filtered/pop/
-
-
-#### FIX ###
-#### FIX ###
-
-# have solved this problem.
-# Find the location of the script "metaSNV_subpopr.R" and move the line 365 above the line 358:
-# Before:
-
- ~/mambaforge/envs/metaSNV/share/metasnv-2.0.4/metaSNV_subpopr.R
- 
-bpParam <- MulticoreParam(workers = min(N.CORES,length(species)),
-                          jobname = "subpopr",
-                          stop.on.error = FALSE,
-                          threshold = "DEBUG",
-                          log = TRUE,
-                          progressbar = printProgressBar,
-                          logdir = paste0(OUT.DIR,"/threadLogs"))
-dir.create(paste0(OUT.DIR,"/threadLogs"), recursive = T, showWarnings = FALSE)
-
-# After:
-
-dir.create(paste0(OUT.DIR,"/threadLogs"), recursive = T, showWarnings = FALSE)
-bpParam <- MulticoreParam(workers = min(N.CORES,length(species)),
-                          jobname = "subpopr",
-                          stop.on.error = FALSE,
-                          threshold = "DEBUG",
-                          log = TRUE,
-                          progressbar = printProgressBar,
-                          logdir = paste0(OUT.DIR,"/threadLogs"))
-
-#### FIX ###
-#### FIX ###
 
 metaSNV_subpopr.R -i NC_010175_metaSNV -p 12
